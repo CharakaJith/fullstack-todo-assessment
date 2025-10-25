70d9ffb2958e3ddd41c81a04a5648b2f01c3fcb2
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2025-10-25 23:19:29 +0530

    client: all tasks display component implemented

---
+++ client/src/components/tasks/AllTaskDisplay.tsx
@@ -0,0 +1,106 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Button } from '@/components/ui/button';
+
+const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+const api = axios.create({
+  baseURL: apiUrl,
+  withCredentials: true,
+});
+
+const AllTaskDisplay: React.FC = () => {
+  const [tasks, setTasks] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const [error, setError] = useState<string[]>([]);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  // get tasks
+  const fetchTasks = async () => {
+    try {
+      setLoading(true);
+
+      const { data } = await api.get('/api/v1/task');
+      const response = data;
+
+      if (response.success) {
+        setTasks(response.response.data.tasks);
+      } else {
+        setError([response.response.data.message || 'Failed to fetch tasks']);
+      }
+    } catch (error: any) {
+      setError([error.message || 'Failed to fetch tasks']);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  // handle error timeout
+  useEffect(() => {
+    if (isError) {
+      const timer = setTimeout(() => {
+        setIsError(false);
+        setError([]);
+      }, 5000); // 5 seconds
+
+      return () => clearTimeout(timer);
+    }
+  }, [isError]);
+
+  return (
+    <div className="flex flex-col gap-4 w-full h-full">
+      {isError &&
+        error.map((msg, idx) => (
+          <div key={idx} className="w-full py-2 bg-red-500 text-white rounded-md text-sm text-center">
+            {msg}
+          </div>
+        ))}
+
+      {tasks.length === 0 ? (
+        <p className="text-center text-gray-500 mt-2">No tasks available</p>
+      ) : (
+        // tas display container
+        <div className="flex flex-col gap-3 overflow-auto h-full pr-2 pt-1 md:pt-7 pb-1 md:pb-7">
+          {tasks.map((task) => (
+            // task box
+            <div key={task.id} className="w-full border border-gray-300 rounded-lg p-4 shadow-sm flex flex-col">
+              {/* task details */}
+              <h3 className="text-lg md:text-xl font-semibold leading-snug mb-1">{task.title}</h3>
+              <p className="text-gray-700 text-sm md:text-base leading-snug mb-2">{task.description}</p>
+              <hr className="border-gray-300 mb-2" />
+
+              {/* task status and action buttons */}
+              <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-2">
+                {/* left side - status and created date */}
+                <div className="flex flex-col w-full md:w-1/2">
+                  <p className="text-sm md:text-sm text-gray-500 leading-snug mb-1">Status: {task.isCompleted ? 'Completed' : 'Pending'}</p>
+                  <p className="text-xs md:text-sm text-gray-400 leading-snug">Created on: {new Date(task.createdAt).toLocaleDateString()}</p>
+                </div>
+
+                {/* right - action buttons */}
+                <div className="flex gap-2 w-full md:w-1/2">
+                  <Button className="flex-1 px-0 py-1 text-xs md:text-sm bg-blue-600 text-white rounded hover:bg-blue-800 cursor-pointer text-center">
+                    Edit
+                  </Button>
+                  <Button className="flex-1 px-0 py-1 text-xs md:text-sm bg-green-600 text-white rounded hover:bg-green-700 cursor-pointer text-center">
+                    Complete
+                  </Button>
+                  <Button className="flex-1 px-0 py-1 text-xs md:text-sm bg-red-600 text-white rounded hover:bg-red-800 cursor-pointer text-center">
+                    Delete
+                  </Button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AllTaskDisplay;
--- client/src/components/tasks/TaskDisplay.tsx
+++ client/src/components/tasks/TaskDisplay.tsx
@@ -1,4 +1,5 @@
 import CreateTaskForm from '@/forms/CreateTaskForm';
+import AllTaskDisplay from './AllTaskDisplay';
 
 const TaskDisplay: React.FC = () => {
   return (
@@ -13,10 +14,8 @@ const TaskDisplay: React.FC = () => {
         <div className="hidden sm:block w-0.5 h-[90%] bg-gray-400 self-center"></div>
 
         {/* right side - task display */}
-        <div className="flex-1 flex items-center justify-center p-2 mt-2 sm:mt-0">
-          <p>
-            <u>display tasks here</u>
-          </p>
+        <div className="flex-1 flex flex-col p-2 mt-2 sm:mt-0 h-full overflow-auto">
+          <AllTaskDisplay />
         </div>
       </div>
     </div>
